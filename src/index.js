11d820f891d59c6020f562a2b291917ce4f998a8
larissamiyaji <natsumi-23@hotmail.com>
2020-04-02 11:46:30 -0300

    Finalização do HTML e CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+//------------------------------------------------------------------------------
+let offsetNumber = document.getElementById('offset2');
+let encode = document.getElementById('mensagemToEncode');
+let encoded = document.getElementById('mensagemEncoded');
+let offsetButton = document.getElementById('offsetButton');
+let encodeButton = document.getElementById('encodeButton');
+let decodeButton = document.getElementById('decodeButton');
+//------------------------------------------------------------------------------
+function cifrar(){
+  document.getElementById('encodeButton').onclick = ;
+  }
+
+
+
+
+
+
+
+//  let mensagemParaCifrar = document.getElementById('mensagemToEncode');
+//  mensagemParaCifrar.innerHTML = 'mensagemEncoded';
+//}
+//function decifrar('mensagemToEncode'){
+//  let mensagemCifrada = document.getElementById('mensagemToEncode');
+//  mensagemCifrada.innerHTML = 'mensagemEncoded';
+//}
