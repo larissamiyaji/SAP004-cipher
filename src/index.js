d31c890028026da0cd34ec08c791cfa49bfefd99
larissamiyaji <natsumi-23@hotmail.com>
2020-04-07 14:01:16 -0300

    Correção de erros do index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import cipher from './cipher.js';
 
 
-function encode(){
+function encode(event){
+  event.preventDefault();
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
   let mensagemEncoded = document.getElementById('mensagemEncoded');
-  mensagemEncoded.innerHTML = mensagem;
+  mensagemEncoded.innerHTML = cipher.encode(offset, string);
 }
 let encodeButton = document.getElementById("encodeButton2");
 encodeButton.addEventListener('click', encode);
 //------------------------------------------------------------------------------
-function decode(){
+function decode(event){
+  event.preventDefault(); //IMPEDE QUE O TEXTO INSERIDO SUMA APÓS UM TEMPO
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
   let mensagemEncoded = document.getElementById('mensagemEncoded');
   mensagemEncoded.innerHTML = mensagem;
@@ -17,8 +19,10 @@ function decode(){
 let decodeButton = document.getElementById("decodeButton2");
 decodeButton.addEventListener('click', decode);
 //------------------------------------------------------------------------------
-function offset(){
+function offset(event){
+  event.preventDefault();
   let offset = Number(document.getElementById('offset2').value);
 }
 let offsetButton = document.getElementById('offset2');
 offsetButton.addEventListener('click', offset);
+//------------------------------------------------------------------------------
