5ba67bad41d5a0de5308caddd6b586b865fd3f05
larissamiyaji <natsumi-23@hotmail.com>
2020-04-07 13:51:12 -0300

    Início da escrita do chipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,26 @@
 const cipher = {
-  //...
+
+//FUNÇÃO ENCODE
+encode: function(offset, string){
+  let mensagemEncoded = " ";
+  let mensagem = string.toUpperCase();
+
+  for (var i = 0; i < mensagem.length; i++) {
+    let letraASC = mensagem[i].charCodeAt(0);
+    let cifrar = ((letraASC - 65 + offset) % 26) + 65;
+    mensagemEncoded += String.fromCharCode(cifrar);
+  } return mensagemEncoded;
+},
+//FUNÇÃO DECODE
+decode: function(offset, string){
+  let mensagemDecoded = " ";
+  let mensagem = string.toUpperCase();
+
+  for (var i = 0; i < mensagem.length; i++) {
+    let letraASC = mensagem[i].charCodeAt(0);
+    let decifrar = ((letraASC - 90 - offset) % 26) + 90;
+    mensagemDecoded += String.fromCharCode(decifrar);
+  } return mensagemDecoded;
+}
 };
 export default cipher;
