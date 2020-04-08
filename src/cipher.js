4e88e0c84088802670b41d5baf631f9ec992da7d
larissamiyaji <natsumi-23@hotmail.com>
2020-04-08 17:00:10 -0300

    Finalização do cipher. Verificar o erro no teste

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,24 @@
 const cipher = {
 
 //FUNÇÃO ENCODE
-encode: function(offset, string){
+encode: function(offset, mensagem){
   let mensagemEncoded = " ";
-  let mensagem = string.toUpperCase();
+  let mensagem1 = mensagem.toUpperCase();
 
-  for (var i = 0; i < mensagem.length; i++) {
-    let letraASC = mensagem[i].charCodeAt(0);
+  for (var i = 0; i < mensagem1.length; i++) {
+    let letraASC = mensagem1[i].charCodeAt(0);
     let cifrar = ((letraASC - 65 + offset) % 26) + 65;
     mensagemEncoded += String.fromCharCode(cifrar);
   } return mensagemEncoded;
 },
 
 //FUNÇÃO DECODE
-decode: function(offset, string){
+decode: function(offset, mensagem){
   let mensagemDecoded = " ";
-  let mensagem = string.toUpperCase();
+  let mensagem1 = mensagem.toUpperCase();
 
-  for (var i = 0; i < mensagem.length; i++) {
-    let letraASC = mensagem[i].charCodeAt(0);
+  for (var i = 0; i < mensagem1.length; i++) {
+    let letraASC = mensagem1[i].charCodeAt(0);
     let decifrar = ((letraASC - 90 - offset) % 26) + 90;
     mensagemDecoded += String.fromCharCode(decifrar);
   } return mensagemDecoded;
