1994fccba87105b316dfe20225127b8cd93273cf
larissamiyaji <natsumi-23@hotmail.com>
2020-04-08 17:01:22 -0300

    Primeira rodagem nos tetes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import cipher from './cipher.js';
 function encode(event){
   event.preventDefault();
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
+  let offset = Number(document.getElementById('offset2').value);
   let mensagemEncoded = document.getElementById('mensagemEncoded');
-  mensagemEncoded.innerHTML = cipher.encode(offset, string);
+  mensagemEncoded.innerHTML = cipher.encode(offset, mensagem);
 }
 let encodeButton = document.getElementById("encodeButton2");
 encodeButton.addEventListener('click', encode);
@@ -12,16 +13,17 @@ encodeButton.addEventListener('click', encode);
 function decode(event){
   event.preventDefault(); //IMPEDE QUE O TEXTO INSERIDO SUMA APÓS UM TEMPO
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
+  let offset = Number(document.getElementById('offset2').value);
   let mensagemEncoded = document.getElementById('mensagemEncoded');
-  mensagemEncoded.innerHTML = mensagem;
+  mensagemEncoded.innerHTML = cipher.decode(offset, mensagem);
 }
 let decodeButton = document.getElementById("decodeButton2");
 decodeButton.addEventListener('click', decode);
 //------------------------------------------------------------------------------
-function offset(event){
+/*function offset(event){
   event.preventDefault();
   let offset = Number(document.getElementById('offset2').value);
 }
 let offsetButton = document.getElementById('offset2');
 offsetButton.addEventListener('click', offset);
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------*/
