a7ca3e5a75686951e29ae19872558dd2767b4b81
larissamiyaji <natsumi-23@hotmail.com>
2020-04-10 22:25:14 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,4 +1,30 @@
 const cipher = {
-  //...
+
+//FUNÇÃO ENCODE
+encode: function(offset, mensagem){
+  let mensagemEncoded = "";
+  let mensagem1 = mensagem.toUpperCase();
+
+  for (var i = 0; i < mensagem1.length; i++) {
+    let letraASC = mensagem1[i].charCodeAt();
+    let cifrar = ((letraASC - 65 + offset) % 26) + 65;
+    mensagemEncoded += String.fromCharCode(cifrar);
+  } return mensagemEncoded;
+},
+
+//FUNÇÃO DECODE
+decode: function(offset, mensagem){
+  let mensagemDecoded = "";
+  let mensagem1 = mensagem.toUpperCase();
+
+  for (var i = 0; i < mensagem1.length; i++) {
+    let letraASC = mensagem1[i].charCodeAt();
+    let decifrar = ((letraASC - 90 - offset) % 26) + 90;
+    mensagemDecoded += String.fromCharCode(decifrar);
+  } return mensagemDecoded;
+}
 };
 export default cipher;
+
+
+
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import cipher from './cipher.js';
 
-
-function encode(){
+function encode(event){
+  event.preventDefault();
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
+  let offset = Number(document.getElementById('offset2').value);
   let mensagemEncoded = document.getElementById('mensagemEncoded');
-  mensagemEncoded.innerHTML = mensagem;
+  mensagemEncoded.innerHTML = cipher.encode(offset, mensagem);
 }
 let encodeButton = document.getElementById("encodeButton2");
 encodeButton.addEventListener('click', encode);
 //------------------------------------------------------------------------------
-function decode(){
+function decode(event){
+  event.preventDefault(); //IMPEDE QUE O TEXTO INSERIDO SUMA APÓS UM TEMPO
   let mensagem = document.getElementById('mensagemToEncode').value.toUpperCase();
+  let offset = Number(document.getElementById('offset2').value);
   let mensagemEncoded = document.getElementById('mensagemEncoded');
-  mensagemEncoded.innerHTML = mensagem;
+  mensagemEncoded.innerHTML = cipher.decode(offset, mensagem);
 }
 let decodeButton = document.getElementById("decodeButton2");
 decodeButton.addEventListener('click', decode);
 //------------------------------------------------------------------------------
-function offset(){
+/*function offset(event){
+  event.preventDefault();
   let offset = Number(document.getElementById('offset2').value);
 }
 let offsetButton = document.getElementById('offset2');
 offsetButton.addEventListener('click', offset);
+//------------------------------------------------------------------------------*/
